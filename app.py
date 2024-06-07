6b83e42b1c328168fb3cbb3eb0bed5514b09b3f4
Neethu-Chandran-S <147372779+Neethu-Chandran-S@users.noreply.github.com>
2024-06-07 17:08:00 +0000

    7

--- app.py
+++ app.py
@@ -5,9 +5,28 @@ import numpy as np
 
 app = Flask(__name__)
 
+prediction_values = pd.read_csv("prediction_values.csv")
+encoded_values = pd.read_csv("tobe_scaled.csv")
+
 @app.route('/')
 def homepage():
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/knowledge_center')
+def knowledge_center():
+    return render_template('Knowledge.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/predict')
+def predict():
+    return render_template('predict.html')
 
 if __name__ == "__main__":
     app.run()
